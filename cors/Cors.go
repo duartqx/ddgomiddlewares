d073a552ace6df32366a9465a2ab977c322df43e
Diego Duarte <duartqx@gmail.com>
2024-02-09 21:45:24 -0300

    cors

---
+++ cors/Cors.go
@@ -0,0 +1,20 @@
+package cors
+
+import "net/http"
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set CORS headers
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+		// If this is a preflight request, respond immediately
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		// Call the next handler in the chain
+		next.ServeHTTP(w, r)
+	})
+}
