97a6170431d93aa6959e9f39bca8b74de2471a0c
Diego Duarte <duartqx@gmail.com>
2024-02-24 23:11:35 -0300

    cors allowed methods *

--- cors/Cors.go
+++ cors/Cors.go
@@ -6,8 +6,12 @@ func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Set CORS headers
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
+		w.Header().Set(
+			"Access-Control-Allow-Headers",
+			"Accept, Content-Type, Content-Length, "+
+				"Accept-Encoding, X-CSRF-Token, Authorization",
+		)
 
 		// If this is a preflight request, respond immediately
 		if r.Method == "OPTIONS" {
