c36d1d25d7ca633ef7b5835b98957f9090f594ed
Diego Duarte <duartqx@gmail.com>
2025-07-08 22:50:34 -0300

    feat: http.Flusher

--- logger/ResponseRecorderWriter.go
+++ logger/ResponseRecorderWriter.go
@@ -13,3 +13,7 @@ func (rr *ResponseRecorderWriter) WriteHeader(status int) {
 	rr.Status = status
 	rr.ResponseWriter.WriteHeader(status)
 }
+
+func (rr *ResponseRecorderWriter) Flush() {
+	rr.ResponseWriter.(http.Flusher).Flush()
+}
