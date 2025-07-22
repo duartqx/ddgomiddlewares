23d4e8591417861e5369f765be0559c8e4b51510
Diego Duarte <duartqx@gmail.com>
2025-07-21 21:52:24 -0300

    With mutations

--- logger/RequestSlogger.go
+++ logger/RequestSlogger.go
@@ -21,32 +21,32 @@ func NewRequestSLogger(id uuid.UUID) *RequestSLogger {
 	return &RequestSLogger{id: id}
 }
 
-func (rl RequestSLogger) WithMethod(method string) RequestSLogger {
+func (rl *RequestSLogger) WithMethod(method string) *RequestSLogger {
 	rl.method = method
 	return rl
 }
 
-func (rl RequestSLogger) WithResult(result any) RequestSLogger {
+func (rl *RequestSLogger) WithResult(result any) *RequestSLogger {
 	rl.result = result
 	return rl
 }
 
-func (rl RequestSLogger) WithStatus(status int) RequestSLogger {
+func (rl *RequestSLogger) WithStatus(status int) *RequestSLogger {
 	rl.status = status
 	return rl
 }
 
-func (rl RequestSLogger) WithSince(since time.Duration) RequestSLogger {
+func (rl *RequestSLogger) WithSince(since time.Duration) *RequestSLogger {
 	rl.since = since
 	return rl
 }
 
-func (rl RequestSLogger) WithPath(path string) RequestSLogger {
+func (rl *RequestSLogger) WithPath(path string) *RequestSLogger {
 	rl.path = path
 	return rl
 }
 
-func (rl RequestSLogger) WithHost(host string) RequestSLogger {
+func (rl *RequestSLogger) WithHost(host string) *RequestSLogger {
 	rl.host = host
 	return rl
 }
